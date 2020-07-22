0ea54b0a876d0237015da720a606c8951e10a445
egaliyando <aaliyando12@gmail.com>
2020-07-22 23:39:50 +0700

    styling maps, add maps admin, sales, supervisor

--- src/pages/Sales/History/index.js
+++ src/pages/Sales/History/index.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Container from "components/Container";
 import Header from "components/Header";
 import MobileNav from "components/Navigation/MobileNav";
 import { Link } from "react-router-dom";
+import L from "leaflet";
+
+const style = {
+  width: "100%",
+  height: "100%",
+};
+
+function Map({ markerPosition }) {
+  // create map
+  const mapRef = useRef(null);
+  useEffect(() => {
+    mapRef.current = L.map("map", {
+      center: [-5.45, 105.26667],
+      zoom: 16,
+      layers: [
+        L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        }),
+      ],
+    });
+  }, []);
+
+  // add marker
+  const markerRef = useRef(null);
+  useEffect(() => {
+    if (markerRef.current) {
+      markerRef.current.setLatLng(markerPosition);
+    } else {
+      markerRef.current = L.marker(markerPosition).addTo(mapRef.current);
+    }
+  }, [markerPosition]);
+
+  return <div id="map" style={style} />;
+}
 
 const list = [
   {
@@ -28,6 +62,12 @@ const list = [
 ];
 
 function History() {
+  //map
+  const [markerPosition, setMarkerPosition] = useState({
+    lat: -5.45,
+    lng: 105.26667,
+  });
+  //map
   return (
     <Container>
       <Header hSales={true} />
@@ -35,7 +75,9 @@ function History() {
       <div className="p-3">
         <p className="text-gray-600">History kunjungan</p>
         {/* MAPS History */}
-        <div className="w-full h-64 bg-gray-400 mt-3 rounded-md"></div>
+        <div className="w-full h-64 bg-gray-400 mt-3 rounded-md">
+          <Map markerPosition={markerPosition} />
+        </div>
         <div className="overflow-y-auto h-64 pb-12">
           {list.map((item) => (
             <Link to="/sales/history/detail">
--- src/pages/Supervisor/Tracking/DetailTrack.js
+++ src/pages/Supervisor/Tracking/DetailTrack.js
@@ -1,8 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Container from "components/Container";
 import Header from "components/Header";
 import MobileNav from "components/Navigation/MobileNav";
 import { Link } from "react-router-dom";
+import L from "leaflet";
+
+const style = {
+  width: "100%",
+  height: "100%",
+};
+
+function Map({ markerPosition }) {
+  // create map
+  const mapRef = useRef(null);
+  useEffect(() => {
+    mapRef.current = L.map("map", {
+      center: [-5.45, 105.26667],
+      zoom: 16,
+      layers: [
+        L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        }),
+      ],
+    });
+  }, []);
+
+  // add marker
+  const markerRef = useRef(null);
+  useEffect(() => {
+    if (markerRef.current) {
+      markerRef.current.setLatLng(markerPosition);
+    } else {
+      markerRef.current = L.marker(markerPosition).addTo(mapRef.current);
+    }
+  }, [markerPosition]);
+
+  return <div id="map" style={style} />;
+}
 
 //list apotik
 const list = [
@@ -69,6 +103,12 @@ const TabItemComponent = ({
 //componrnt tab
 
 function Tracking() {
+  //map
+  const [markerPosition, setMarkerPosition] = useState({
+    lat: -5.45,
+    lng: 105.26667,
+  });
+  //map
   return (
     <>
       <div className="bg-white rounded-lg grid grid-cols-2 gap-5 justify-between mt-3 p-3">
@@ -82,7 +122,9 @@ function Tracking() {
           <p className="text-gray-600 tex-md">Jl. Abdul Muis no 06 </p>
         </div>
       </div>
-      <div style={{ height: "22rem" }} className="rounded-lg w-full bg-gray-500 mt-3"></div>
+      <div style={{ height: "21rem" }} className="rounded-lg w-full bg-gray-500 mt-2">
+        <Map markerPosition={markerPosition} />
+      </div>
     </>
   );
 }
