aa653859e7c86a6da5720b405df3096443e51655
egaliyando <aaliyando12@gmail.com>
2021-11-06 09:49:29 +0700

    FIlter salex

--- src/pages/Sales/History/index.js
+++ src/pages/Sales/History/index.js
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from "react";
-import Container from "components/Container";
-import Header from "components/Header";
-import MobileNav from "components/Navigation/MobileNav";
-import axios from "configs";
-import moment from "moment";
-import { Link, Redirect } from "react-router-dom";
-import { useSelector } from "react-redux";
+import React, { useState, useEffect } from 'react';
+import Container from 'components/Container';
+import Header from 'components/Header';
+import MobileNav from 'components/Navigation/MobileNav';
+import axios from 'configs';
+import moment from 'moment';
+import { Link, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 //maps
-import { Map, TileLayer, Marker, Popup, Circle } from "react-leaflet";
-import { iconSales, iconApotik } from "components/MapComponent/IconMarker";
-import DatePicker from "react-datepicker";
+import { Map, TileLayer, Marker, Popup, Circle } from 'react-leaflet';
+import { iconSales, iconApotik } from 'components/MapComponent/IconMarker';
+import DatePicker from 'react-datepicker';
 
 function History(props) {
   const token = useSelector((state) => state.users.token);
   //maps component
   //deklarasi state tagging sales history
   const [arrayTag, setArrayTag] = useState([]);
-  console.log("arrayTag");
+  console.log('arrayTag');
   console.log(arrayTag);
   //deklarasi data tagging all trip/apotik
   const [dataApotik, setDataApotik] = useState([]);
@@ -34,7 +34,7 @@ function History(props) {
 
   //deklarasi state list history
   const [listHistory, setListHistory] = useState([]);
-  console.log("listHistory");
+  console.log('listHistory');
   console.log(listHistory);
 
   //deklarasi id sales dari local storage
@@ -81,7 +81,7 @@ function History(props) {
         },
       })
       .then((res) => {
-        console.log("res kuyyy");
+        console.log('res kuyyy');
         console.log(res);
         let Arrays = [...listHistory];
         let TagMap = [...arrayTag];
@@ -91,8 +91,8 @@ function History(props) {
             res.data.tracking[i].apotik.address,
             res.data.tracking[i].id,
             res.data.tracking[i].apotik.image,
-            moment(res.data.tracking[i].createdAt).format("DD-MM-YYYY"),
-            moment(res.data.tracking[i].createdAt).format("LLLL"),
+            moment(res.data.tracking[i].createdAt).format('DD-MM-YYYY'),
+            moment(res.data.tracking[i].createdAt).format('LLLL'),
           ]);
           TagMap.push([
             parseFloat(res.data.tracking[i].apotik.lat),
@@ -100,8 +100,8 @@ function History(props) {
             res.data.tracking[i].apotik.name,
             res.data.tracking[i].apotik.address,
             res.data.tracking[i].apotik.image,
-            moment(res.data.tracking[i].createdAt).format("DD-MM-YYYY"),
-            moment(res.data.tracking[i].createdAt).format("LLLL"),
+            moment(res.data.tracking[i].createdAt).format('DD-MM-YYYY'),
+            moment(res.data.tracking[i].createdAt).format('LLLL'),
           ]);
         }
         //list history
@@ -117,7 +117,7 @@ function History(props) {
   //untuk cek today
   const dates = new Date();
   //ubah format tanggalnya
-  const dateFormat = moment(day).format("DD-MM-YYYY");
+  const dateFormat = moment(day).format('DD-MM-YYYY');
 
   //CHANGE DAY
   const handleDay = (date) => {
@@ -128,7 +128,7 @@ function History(props) {
     getData();
     getHistory();
   }, []);
-  if (token === "") {
+  if (token === '') {
     return <Redirect to="/" />;
   }
   return (
@@ -139,22 +139,18 @@ function History(props) {
         <p className="text-gray-600 font-bold">History kunjungan</p>
         {/* MAPS History */}
         <div className="w-full h-64 bg-gray-400 mt-3 rounded-md">
-          <Map style={{ width: "100%", height: "100%" }} center={position} zoom={dataMaps.zoom}>
+          <Map style={{ width: '100%', height: '100%' }} center={position} zoom={dataMaps.zoom}>
             <TileLayer
               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             {dataApotik
-              .filter((days) => days.includes(dateFormat))
+              // .filter((days) => days.includes(dateFormat))
               .map((data, i) => {
                 return (
                   <Marker position={[data[0], data[1]]} icon={iconApotik} key={i}>
                     <Popup>
-                      <img
-                        className="h-12 w-12 mb-3"
-                        src={`${process.env.REACT_APP_HOST_HEROKU}${[data[4]]}`}
-                        alt="img"
-                      />
+                      <img className="h-12 w-12 mb-3" src={`${process.env.REACT_APP_HOST_HEROKU}${[data[4]]}`} alt="img" />
                       <span className="text-xl">{data[2]}</span> <br /> {data[3]}
                       <br /> <p className="text-md">Status : Belum Dikunjungi</p>
                     </Popup>
@@ -163,17 +159,13 @@ function History(props) {
                 );
               })}
             {arrayTag
-              .filter((days) => days.includes(dateFormat))
+              // .filter((days) => days.includes(dateFormat))
               .map((data, i) => {
                 return (
                   <Marker position={[data[0], data[1]]} icon={iconSales} key={i}>
                     <Popup>
                       <div style={{ width: 200, height: 160 }} className="overflow-y-scroll">
-                        <img
-                          className="h-12 w-12 mb-3"
-                          src={`${process.env.REACT_APP_HOST_HEROKU}${[data[4]]}`}
-                          alt="img"
-                        />
+                        <img className="h-12 w-12 mb-3" src={`${process.env.REACT_APP_HOST_HEROKU}${[data[4]]}`} alt="img" />
                         <span className="text-sm">{data[2]}</span> <br /> {data[3]}
                         <br />
                         <br />
@@ -194,17 +186,13 @@ function History(props) {
             placeholderText="Select a day"
           />
           {listHistory
-            .filter((days) => days.includes(dateFormat))
+            // .filter((days) => days.includes(dateFormat))
             .map((item, i) => {
               return (
                 <Link to={`/sales/history/detail/${id}/${item[2]}`} key={i}>
                   <div className="mt-2">
                     <div className="w-full p-2 rounded-lg bg-white h-auto flex">
-                      <img
-                        className="h-16 w-16 mb-3"
-                        src={`${process.env.REACT_APP_HOST_HEROKU}${item[3]}`}
-                        alt="img"
-                      />
+                      <img className="h-16 w-16 mb-3" src={`${process.env.REACT_APP_HOST_HEROKU}${item[3]}`} alt="img" />
                       <div className="ml-3">
                         <p className="font-bold text-gray-600">{item[0]}</p>
                         <p className="text-xs text-gray-600">{item[1]}</p>
@@ -219,7 +207,7 @@ function History(props) {
           ))} */}
         </div>
       </div>
-      <div style={{ width: "-webkit-fill-available" }} className="fixed bg-white bottom-0 max-w-md">
+      <div style={{ width: '-webkit-fill-available' }} className="fixed bg-white bottom-0 max-w-md">
         <MobileNav isSales={true} {...props} />
       </div>
     </Container>
